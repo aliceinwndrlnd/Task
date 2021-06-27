ea0378653b1f28ce8551fcadcbac18df48bdceb4
Alesya Yurkevich <lesja_1_gv@mail.ru>
2021-06-28 00:45:15 +0300

    Taski do 10

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String t = "Task";
+	int i = 1;
+	while (i <= 10) {
+	System.out.println(t + i);
+	i++;
+	}
+    }
+}
